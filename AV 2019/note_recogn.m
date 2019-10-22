108eed6f0676f9b38c8f845246ce947df781ac31
kgoulio <53231402+kgoulio@users.noreply.github.com>
2019-10-22 12:39:42 +0300

    Add files via upload

---
+++ AV 2019/note_recogn.m
@@ -0,0 +1,63 @@
+%recobj=audiorecorder;
+%recordblocking(recobj, 3);
+%y=getaudiodata(recobj);
+clc
+clear all 
+close all
+hold off
+
+ref_table= readtable('ScaleFreqs.xls');
+%freq_names(1:107)=ref_table(:,1)
+
+
+%[y,fs]=audioread('A PIANO.mp3');
+[y,fs]=audioread('A3.mp3');
+%[y,fs]=audioread('c4.mp3');
+%[y,fs]=audioread('mix.mp3');
+N=length(y);
+
+
+
+Nfft=N; %Nfft=time(duration)*FS
+%plot(y);
+z=fft(y,Nfft);
+x=abs(fft(y,Nfft));
+for i=1:N
+    af(i)=i*fs/N;
+end
+%plot(af,x)
+plot(af(1:Nfft),x)
+%descend=sort(z, 'descend');
+[M,I]=max(x);
+af_max=I*fs/N;
+
+
+a=2^(1/12);
+f_ref=440; %Perfect pitch A
+
+%n=log(af_max/f_ref)/log(a) %number of semitones from A4
+
+
+i=1;
+while i<=107
+    if af_max == ref_table(i,2)
+        name=ref_table(i,1)
+        fprintf('Note is ')
+        fprintf(name)
+        break
+    elseif af_max>ref_table(i,2)
+         i=i+1;
+    else
+        dif=af_max-red_table(i-1,2);
+        
+        fprintf('Note is ')
+        fprintf(dif)
+        fprintf('sharper than')
+        fprintf(ref_table(i-1,1))
+        break
+    end
+end
+
+    
+
+
